f75d0615af757ae4401cb44b28f55b8d1b68ecef
dnaxruz <dan_uchiha@comunidad.unam.mx>
2025-05-22 06:24:25 -0600

    vista y control

---
+++ control/ControlProductos.java
@@ -0,0 +1,11 @@
+package control;
+
+import vista.VistaProductos;
+
+public class ControlProductos {
+    
+    public static void main(String... args){
+
+         VistaProductos vista = new VistaProductos();
+    }
+}
--- vista/VistaProductos.java
+++ vista/VistaProductos.java
@@ -7,10 +7,12 @@ public class VistaProductos{
     private JFrame frame;
 
     public VistaProductos(){
+        super();
         frame = new JFrame("Hello Name");
-
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
         frame.setSize(500,500);
+        frame.setVisible(true);
     }
+
+
 }
